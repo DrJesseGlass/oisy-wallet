37bef392ed4813e48c94f9cee783a7d0f787bd84
DrJesseGlass <dr.jesse.glass@gmail.com>
2025-11-17 14:48:53 -0500

    csp for local

--- scripts/build.csp.mjs
+++ scripts/build.csp.mjs
@@ -201,9 +201,9 @@ const updateCSP = (indexHtml) => {
 		'https://burned-little-dinghy.solana-mainnet.quiknode.pro wss://burned-little-dinghy.solana-mainnet.quiknode.pro wss://burned-little-dinghy.solana-testnet.quiknode.pro wss://burned-little-dinghy.solana-devnet.quiknode.pro';
 	const solanaApiConnectSrc = `${solanaRpcApiConnectSrc} ${solanaAlchemyApiConnectSrc} ${solanaQuicknodeApiConnectSrc}`;
 
-	const allConnectSrc =
-		'https://ic0.app https://icp0.io https://icp-api.io' +
-		` ${ethMainnetConnectSrc} ${ethSepoliaConnectSrc} ${evmConnectSrc} ${infuraConnectSrc} ${walletConnectSrc} ${onramperConnectFrameSrc} ${blockstreamApiConnectSrc} ${blockchainApiConnectSrc} ${coingeckoApiConnectSrc} ${solanaApiConnectSrc} ${plausibleApiConnectSrc} ${kongSwapApiConnectSrc} ${paraswapApiConnectSrc}`;
+    const allConnectSrc =
+        'https://ic0.app https://icp0.io https://icp-api.io http://localhost:4943 http://*.localhost:4943' +
+        ` ${ethMainnetConnectSrc} ${ethSepoliaConnectSrc} ${evmConnectSrc} ${infuraConnectSrc} ${walletConnectSrc} ${onramperConnectFrameSrc} ${blockstreamApiConnectSrc} ${blockchainApiConnectSrc} ${coingeckoApiConnectSrc} ${solanaApiConnectSrc} ${plausibleApiConnectSrc} ${kongSwapApiConnectSrc} ${paraswapApiConnectSrc}`;
 
 	// TODO: remove once the feature has been completed
 	const NFTS_ENABLED = process.env.VITE_NFTS_ENABLED;
